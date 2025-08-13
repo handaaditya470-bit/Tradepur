050012ae8d2a612524588e49a472f95b889b411e
Anmol-Bholu <anmolsdeep70@gmail.com>
2025-08-13 20:15:48 +0530

    Update settings.py

--- TradePur/settings.py
+++ TradePur/settings.py
@@ -37,7 +37,11 @@ SECRET_KEY = "django-insecure-d)cfhul3gp#qjpfe)^vidsy&dw7$za@z9l85dft&(%ka(w#tza
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "tradepur.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
