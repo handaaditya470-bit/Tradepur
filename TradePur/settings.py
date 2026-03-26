b38952ba6383b16bb3a428294e057b4bb3df3482
Aditya Handa <handaaditya470@gmail.com>
2026-03-27 01:08:49 +0530

    fix allowed hosts

--- TradePur/settings.py
+++ TradePur/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-d)cfhul3gp#qjpfe)^vid
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tradepur-twrp.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
