4f980ecee01ee94ac812897d7b70610525e666e4
Aditya Handa <handaaditya470@gmail.com>
2026-03-27 14:14:22 +0530

    switch to smtp port 465

--- TradePur/settings.py
+++ TradePur/settings.py
@@ -9,8 +9,9 @@ CSRF_TRUSTED_ORIGINS = [
 # Email config - reads from Render environment variables
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'tradepur4@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 
